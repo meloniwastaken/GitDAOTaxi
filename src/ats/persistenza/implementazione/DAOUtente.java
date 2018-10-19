ee7041edaceaec7b63d4f70de7da7019ca802ab1
Andrea Marotto <andrea.marotto91@gmail.com>
2018-10-19 16:03:22 +0200

    prova remoto

--- src/ats/persistenza/implementazione/DAOUtente.java
+++ src/ats/persistenza/implementazione/DAOUtente.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import ats.modello.Amministratore;
@@ -16,8 +17,48 @@ public class DAOUtente implements IDAOUtente {
 
 	@Override
 	public List<Utente> findAll() throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Utente> utenti = new ArrayList<Utente>(0);
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = DataSource.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT * FROM UTENTE");
+			resultSet = statement.executeQuery();
+			
+			while(resultSet.next()) {
+				Integer ruolo = resultSet.getInt("RUOLO");
+				Utente utente = null;
+				if(ruolo==1)
+					utente = new Amministratore();
+				else if (ruolo==2)
+					utente = new Autista();
+				else 
+					utente = new Cliente();
+				utente.setId(resultSet.getLong(1));
+				utente.setNome(resultSet.getString(2));
+				utente.setCognome(resultSet.getString(3));
+				utente.setCodiceFiscale(resultSet.getString(4));
+				utente.setDataDiNascita(resultSet.getDate(5));
+				utente.setIndirizzo(resultSet.getString(6));
+				utente.setTelefono(resultSet.getString(7));
+				utente.setEmail(resultSet.getString(8));
+				utente.setUsername(resultSet.getString(9));
+				utente.setPassword(resultSet.getString(10));
+				utenti.add(utente);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new DAOException(e.getMessage(), e);
+		}
+		finally {
+			DataSource.getInstance().close(resultSet);
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+		return utenti;
 	}
 
 	@Override
