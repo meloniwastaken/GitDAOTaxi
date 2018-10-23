939bb5f248fef6471eb68f7a098346ad5d58438f
meloniwastaken <meloni5@live.it>
2018-10-23 10:27:01 +0200

    aggiunta stipendio e find varie

--- src/ats/persistenza/implementazione/DAOUtente.java
+++ src/ats/persistenza/implementazione/DAOUtente.java
@@ -18,6 +18,14 @@ public class DAOUtente implements IDAOUtente {
 	@Override
 	public List<Utente> findAll() throws DAOException {
 		List<Utente> utenti = new ArrayList<Utente>(0);
+		utenti.addAll(this.findAllAmministratore());
+		utenti.addAll(this.findAllCliente());
+		utenti.addAll(this.findAllAutista());
+		return utenti;
+	}
+	
+	public List<Cliente> findAllCliente() throws DAOException {
+		List<Cliente> utenti = new ArrayList<Cliente>(0);
 		
 		Connection connection = null;
 		PreparedStatement statement = null;
@@ -25,29 +33,102 @@ public class DAOUtente implements IDAOUtente {
 		
 		try {
 			connection = DataSource.getInstance().getConnection();
-			statement = connection.prepareStatement("SELECT * FROM UTENTE");
+			statement = connection.prepareStatement("SELECT * FROM UTENTE WHERE RUOLO = 3");
 			resultSet = statement.executeQuery();
 			
 			while(resultSet.next()) {
-				Integer ruolo = resultSet.getInt("RUOLO");
-				Utente utente = null;
-				if(ruolo==1)
-					utente = new Amministratore();
-				else if (ruolo==2)
-					utente = new Autista();
-				else 
-					utente = new Cliente();
-					utente.setId(resultSet.getLong(1));
-					utente.setNome(resultSet.getString(2));
-					utente.setCognome(resultSet.getString(3));
-					utente.setCodiceFiscale(resultSet.getString(4));
-					utente.setDataDiNascita(resultSet.getDate(5));
-					utente.setIndirizzo(resultSet.getString(6));
-					utente.setTelefono(resultSet.getString(7));
-					utente.setEmail(resultSet.getString(8));
-					utente.setUsername(resultSet.getString(9));
-					utente.setPassword(resultSet.getString(10));
-					utenti.add(utente);
+				Cliente utente = new Cliente();
+				
+				utente.setId(resultSet.getLong(1));
+				utente.setNome(resultSet.getString(2));
+				utente.setCognome(resultSet.getString(3));
+				utente.setCodiceFiscale(resultSet.getString(4));
+				utente.setDataDiNascita(resultSet.getDate(5));
+				utente.setIndirizzo(resultSet.getString(6));
+				utente.setTelefono(resultSet.getString(7));
+				utente.setEmail(resultSet.getString(8));
+				utente.setUsername(resultSet.getString(9));
+				utente.setPassword(resultSet.getString(10));
+				utenti.add(utente);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new DAOException(e.getMessage(), e);
+		}
+		finally {
+			DataSource.getInstance().close(resultSet);
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+		return utenti;
+	}
+	
+	public List<Amministratore> findAllAmministratore() throws DAOException {
+		List<Amministratore> utenti = new ArrayList<Amministratore>(0);
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = DataSource.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT * FROM UTENTE WHERE RUOLO = 1");
+			resultSet = statement.executeQuery();
+			
+			while(resultSet.next()) {
+				Amministratore utente = new Amministratore();
+				
+				utente.setId(resultSet.getLong(1));
+				utente.setNome(resultSet.getString(2));
+				utente.setCognome(resultSet.getString(3));
+				utente.setCodiceFiscale(resultSet.getString(4));
+				utente.setDataDiNascita(resultSet.getDate(5));
+				utente.setIndirizzo(resultSet.getString(6));
+				utente.setTelefono(resultSet.getString(7));
+				utente.setEmail(resultSet.getString(8));
+				utente.setUsername(resultSet.getString(9));
+				utente.setPassword(resultSet.getString(10));
+				utenti.add(utente);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new DAOException(e.getMessage(), e);
+		}
+		finally {
+			DataSource.getInstance().close(resultSet);
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+		return utenti;
+	}
+	
+	public List<Autista> findAllAutista() throws DAOException {
+		List<Autista> utenti = new ArrayList<Autista>(0);
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = DataSource.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT * FROM UTENTE RIGHT JOIN STIPENDIO ON UTENTE.ID = STIPENDIO.ID");
+			resultSet = statement.executeQuery();
+			
+			while(resultSet.next()) {
+				Autista utente = new Autista();
+				
+				utente.setId(resultSet.getLong(1));
+				utente.setNome(resultSet.getString(2));
+				utente.setCognome(resultSet.getString(3));
+				utente.setCodiceFiscale(resultSet.getString(4));
+				utente.setDataDiNascita(resultSet.getDate(5));
+				utente.setIndirizzo(resultSet.getString(6));
+				utente.setTelefono(resultSet.getString(7));
+				utente.setEmail(resultSet.getString(8));
+				utente.setUsername(resultSet.getString(9));
+				utente.setPassword(resultSet.getString(10));
+				utente.setStipendio(resultSet.getDouble(13));
+				utenti.add(utente);
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
--- src/ats/persistenza/interfacce/IDAOUtente.java
+++ src/ats/persistenza/interfacce/IDAOUtente.java
@@ -2,11 +2,17 @@ package ats.persistenza.interfacce;
 
 import java.util.List;
 
+import ats.modello.Amministratore;
+import ats.modello.Autista;
+import ats.modello.Cliente;
 import ats.modello.Utente;
 import ats.persistenza.implementazione.DAOException;
 
 public interface IDAOUtente {
 	public abstract List<Utente> findAll() throws DAOException;
+	public abstract List<Amministratore> findAllAmministratore() throws DAOException;
+	public abstract List<Cliente> findAllCliente() throws DAOException;
+	public abstract List<Autista> findAllAutista() throws DAOException;
 	public abstract Utente findById(Long id) throws DAOException;
 	public abstract Utente findByUsernameAndPassword(String username, String password) throws DAOException;
 	public abstract void update(Utente u) throws DAOException;
