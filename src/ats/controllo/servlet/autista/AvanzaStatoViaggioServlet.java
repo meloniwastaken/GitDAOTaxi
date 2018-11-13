1c0bc9edc178779c139961e188486f3de94ee9ad
pisanov26 <pisanov26@gmail.com>
2018-11-13 08:59:19 +0100

    Pulizia testo
    Pulizia testo in tutte le servlet dell'autista

--- src/ats/controllo/servlet/autista/AvanzaStatoViaggioServlet.java
+++ src/ats/controllo/servlet/autista/AvanzaStatoViaggioServlet.java
@@ -8,47 +8,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ats.modello.Viaggio;
-import ats.modello.Autista;
 import ats.modello.Taxi;
-import ats.persistenza.implementazione.DAOAutista;
 import ats.persistenza.implementazione.DAOException;
 import ats.persistenza.implementazione.DAOTaxi;
 import ats.persistenza.implementazione.DAOViaggio;
-import ats.persistenza.interfacce.IDAOAutista;
 import ats.persistenza.interfacce.IDAOTaxi;
 import ats.persistenza.interfacce.IDAOViaggio;
 
-/**
- * Servlet implementation class AvanzaStatoViaggioServlet
- */
 @WebServlet("/autista/avanzaStatoViaggio")
 public class AvanzaStatoViaggioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AvanzaStatoViaggioServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public AvanzaStatoViaggioServlet() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
 		Long idViaggio = Long.parseLong(request.getParameter("idViaggio"));
 		IDAOViaggio daoViaggio = new DAOViaggio();
 		IDAOTaxi daoTaxi = new DAOTaxi();
 		Viaggio v = null;
-		try {	
+		try {
 			v = daoViaggio.findById(idViaggio);
-			if(v.getStato()!=4) {
-				v.setStato(v.getStato()+1);
+			if (v.getStato() != 4) {
+				v.setStato(v.getStato() + 1);
 				daoViaggio.update(v);
 			}
-				
-			if(v.getStato()==2) {
+			if (v.getStato() == 2) {
 				Long id = (Long) request.getSession().getAttribute("id");
 				Taxi t = daoTaxi.findByIdAutista(id);
 				t.setDisponibile(true);
@@ -56,8 +42,7 @@ public class AvanzaStatoViaggioServlet extends HttpServlet {
 				System.out.println(t);
 				daoTaxi.update(t);
 			}
-			
-			if(v.getStato()==3) {
+			if (v.getStato() == 3) {
 				Long id = (Long) request.getSession().getAttribute("id");
 				Taxi t = daoTaxi.findByIdAutista(id);
 				System.out.println("stato 3");
@@ -65,9 +50,7 @@ public class AvanzaStatoViaggioServlet extends HttpServlet {
 				t.setDisponibile(false);
 				daoTaxi.update(t);
 			}
-			
-			
-			if(v.getStato()==4) {
+			if (v.getStato() == 4) {
 				Long id = (Long) request.getSession().getAttribute("id");
 				Taxi t = daoTaxi.findByIdAutista(id);
 				System.out.println("stato 4");
@@ -75,8 +58,6 @@ public class AvanzaStatoViaggioServlet extends HttpServlet {
 				t.setDisponibile(true);
 				daoTaxi.update(t);
 			}
-			
-			
 		} catch (DAOException e) {
 			System.out.println(e.getMessage());
 		}
