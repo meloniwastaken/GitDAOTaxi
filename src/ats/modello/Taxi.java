b40d74024b971a0b04cfd5158c5d5e5804677deb
pisanov26 <pisanov26@gmail.com>
2018-11-13 09:07:07 +0100

    Ordinamento testo
    Ordinamento testo in tutti i modelli

--- src/ats/modello/Taxi.java
+++ src/ats/modello/Taxi.java
@@ -10,67 +10,84 @@ public class Taxi {
 	private Double prezzoPerKilometro;
 	private Boolean disponibile;
 	private Autista autista;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public String getModello() {
 		return modello;
 	}
+
 	public void setModello(String modello) {
 		this.modello = modello;
 	}
+
 	public String getTarga() {
 		return targa;
 	}
+
 	public void setTarga(String targa) {
 		this.targa = targa;
 	}
+
 	public Integer getAnnoDiImmatricolazione() {
 		return annoDiImmatricolazione;
 	}
+
 	public void setAnnoDiImmatricolazione(Integer annoDiImmatricolazione) {
 		this.annoDiImmatricolazione = annoDiImmatricolazione;
 	}
+
 	public Integer getPosti() {
 		return posti;
 	}
+
 	public void setPosti(Integer posti) {
 		this.posti = posti;
 	}
+
 	public Double getPrezzoPerKilometro() {
 		return prezzoPerKilometro;
 	}
+
 	public void setPrezzoPerKilometro(Double prezzoPerKilometro) {
 		this.prezzoPerKilometro = prezzoPerKilometro;
 	}
+
 	public Boolean getDisponibile() {
 		return disponibile;
 	}
+
 	public void setDisponibile(Boolean disponibile) {
 		this.disponibile = disponibile;
 	}
+
 	public Autista getAutista() {
 		return autista;
 	}
+
 	public void setAutista(Autista autista) {
 		this.autista = autista;
 	}
+
 	@Override
 	public String toString() {
 		return "Taxi [id=" + id + ", marca=" + marca + ", modello=" + modello + ", targa=" + targa
 				+ ", annoDiImmatricolazione=" + annoDiImmatricolazione + ", posti=" + posti + ", prezzoPerKilometro="
 				+ prezzoPerKilometro + ", disponibile=" + disponibile + ", autista=" + autista.getId() + "]";
 	}
-	
-	
+
 }
--- src/ats/modello/Viaggio.java
+++ src/ats/modello/Viaggio.java
@@ -14,78 +14,101 @@ public class Viaggio {
 	private Double prezzo;
 	private Integer stato;
 	private Integer feedback;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Autista getAutista() {
 		return autista;
 	}
+
 	public void setAutista(Autista autista) {
 		this.autista = autista;
 	}
+
 	public Taxi getTaxi() {
 		return taxi;
 	}
+
 	public void setTaxi(Taxi taxi) {
 		this.taxi = taxi;
 	}
+
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
 	}
+
 	public String getPartenza() {
 		return partenza;
 	}
+
 	public void setPartenza(String partenza) {
 		this.partenza = partenza;
 	}
+
 	public String getDestinazione() {
 		return destinazione;
 	}
+
 	public void setDestinazione(String destinazione) {
 		this.destinazione = destinazione;
 	}
+
 	public Double getKilometri() {
 		return kilometri;
 	}
+
 	public void setKilometri(Double kilometri) {
 		this.kilometri = kilometri;
 	}
+
 	public Double getPrezzo() {
 		return prezzo;
 	}
+
 	public void setPrezzo(Double prezzo) {
 		this.prezzo = prezzo;
 	}
+
 	public Integer getStato() {
 		return stato;
 	}
+
 	public void setStato(Integer stato) {
 		this.stato = stato;
 	}
+
 	public Integer getFeedback() {
 		return feedback;
 	}
+
 	public void setFeedback(Integer feedback) {
 		this.feedback = feedback;
 	}
+
 	@Override
 	public String toString() {
-		return "Viaggio [id=" + id + ", autista=" + autista.getId() + ", taxi=" + taxi.getId() + ", cliente=" + cliente.getId() + ", data="
-				+ data + ", partenza=" + partenza + ", destinazione=" + destinazione + ", kilometri=" + kilometri
-				+ ", prezzo=" + prezzo + ", stato=" + stato + ", feedback=" + feedback + "]";
+		return "Viaggio [id=" + id + ", autista=" + autista.getId() + ", taxi=" + taxi.getId() + ", cliente="
+				+ cliente.getId() + ", data=" + data + ", partenza=" + partenza + ", destinazione=" + destinazione
+				+ ", kilometri=" + kilometri + ", prezzo=" + prezzo + ", stato=" + stato + ", feedback=" + feedback
+				+ "]";
 	}
-	
+
 }
